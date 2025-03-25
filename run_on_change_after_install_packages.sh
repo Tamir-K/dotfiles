e908e1a4fb99bdb7ceca6eba006a25e8dfc50993
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-03-25 20:56:16 +0200

    Add megasync repo

--- run_on_change_after_install_packages.sh
+++ run_on_change_after_install_packages.sh
@@ -17,6 +17,8 @@ readonly PACKAGES=(
     signal-desktop
     codium
     prismlauncher
+    megasync
+    nemo-megasync
 )
 readonly SIGNAL_KEY_URL="https://updates.signal.org/desktop/apt/keys.asc"
 readonly SIGNAL_REPO="https://updates.signal.org/desktop/apt xenial main"
@@ -24,11 +26,12 @@ readonly VSCODIUM_KEY_URL="https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/
 readonly VSCODIUM_REPO="https://download.vscodium.com/debs vscodium main"
 readonly MPR_KEY_URL="https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub"
 readonly MPR_REPO="https://proget.makedeb.org prebuilt-mpr ${UBUNTU_CODENAME}"
-readonly MEGA_URL="https://mega.nz/linux/repo/xUbuntu_${UBUNTU_RELEASE}/amd64/megasync-xUbuntu_${UBUNTU_RELEASE}_amd64.deb"
+readonly MEGA_KEY_URL="https://mega.nz/keys/MEGA_signing.key"
+readonly MEGA_REPO="https://mega.nz/linux/repo/xUbuntu_${UBUNTU_RELEASE}/ ./"
 readonly ZOOM_URL="https://zoom.us/client/latest/zoom_amd64.deb"
 
 # Functions
-add_deb_repository() {
+add_apt_repository() {
     local repo_name=$1 repo_entry=$2 keyring_name=$3 key_url=$4
     curl -fsSL "$key_url" | gpg --dearmor | sudo tee "${KEYRING_DIR}/${keyring_name}-keyring.gpg" > /dev/null
     echo "deb [signed-by=${KEYRING_DIR}/${keyring_name}-keyring.gpg ] $repo_entry" | sudo tee "${SOURCE_LIST_DIR}/${repo_name}.list" > /dev/null
@@ -37,17 +40,17 @@ add_deb_repository() {
 install_deb_from_url() {
     local package_url=$1
     curl -fsSL -O --create-dirs --output-dir "${TEMP_DEB_DIR}" "$package_url"
-    sudo apt-get install -y "${TEMP_DEB_DIR}/${basename "$package_url"}"
+    sudo apt-get install -y "${TEMP_DEB_DIR}/$(basename "$package_url")"
 }
 
 main() {
-    add_deb_repository "signal-xenial" "$SIGNAL_REPO" "signal-desktop" "$SIGNAL_KEY_URL"
-    add_deb_repository "vscodium" "$VSCODIUM_REPO" "vscodium-archive" "$VSCODIUM_KEY_URL"
-    add_deb_repository "prebuilt-mpr" "$MPR_REPO" "prebuilt-mpr-archive" "$MPR_KEY_URL"
+    add_apt_repository "signal-xenial" "$SIGNAL_REPO" "signal-desktop" "$SIGNAL_KEY_URL"
+    add_apt_repository "vscodium" "$VSCODIUM_REPO" "vscodium-archive" "$VSCODIUM_KEY_URL"
+    add_apt_repository "prebuilt-mpr" "$MPR_REPO" "prebuilt-mpr-archive" "$MPR_KEY_URL"
+    add_apt_repository "megasync" "$MEGA_REPO" "meganz-archive" "$MEGA_KEY_URL"
 
     sudo apt-get update && sudo apt-get install -y "${PACKAGES[@]}"
 
-    install_deb_from_url "$MEGA_URL"
     install_deb_from_url "$ZOOM_URL"
 
     pipx ensurepath && pipx install protonup && protonup -y
