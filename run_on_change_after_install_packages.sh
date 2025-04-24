62ec0a301ade08e6906c46c74e1928a1b3790bf2
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-04-25 01:57:37 +0300

    Cleanup

--- run_on_change_after_install_packages.sh
+++ run_on_change_after_install_packages.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
+source /etc/upstream-release/lsb-release # Get Ubuntu upstream info for Linux Mint
+
 # Constants
 readonly KEYRING_DIR="/usr/share/keyrings"
 readonly SOURCE_LIST_DIR="/etc/apt/sources.list.d"
 readonly TEMP_DEB_DIR="/tmp/debfiles"
-readonly UBUNTU_RELEASE=$(grep '^DISTRIB_RELEASE=' /etc/upstream-release/lsb-release | cut -d'=' -f2) # Get Ubuntu upstream version for Linux Mint
-readonly UBUNTU_CODENAME=$(grep '^DISTRIB_CODENAME=' /etc/upstream-release/lsb-release | cut -d'=' -f2) # Get Ubuntu upstream codename for Linux Mint
+readonly REPO_FORMAT="deb [arch=amd64 signed-by=${KEYRING_DIR}/%s-keyring.gpg] %s"
 readonly PACKAGES=(
     git
     virtualbox
@@ -25,16 +26,16 @@ readonly SIGNAL_REPO="https://updates.signal.org/desktop/apt xenial main"
 readonly VSCODIUM_KEY_URL="https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg"
 readonly VSCODIUM_REPO="https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main"
 readonly MPR_KEY_URL="https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub"
-readonly MPR_REPO="https://proget.makedeb.org prebuilt-mpr ${UBUNTU_CODENAME}"
+readonly MPR_REPO="https://proget.makedeb.org prebuilt-mpr ${DISTRIB_CODENAME}"
 readonly MEGA_KEY_URL="https://mega.nz/keys/MEGA_signing.key"
-readonly MEGA_REPO="https://mega.nz/linux/repo/xUbuntu_${UBUNTU_RELEASE}/ ./"
+readonly MEGA_REPO="https://mega.nz/linux/repo/xUbuntu_${DISTRIB_RELEASE}/ ./"
 readonly ZOOM_URL="https://zoom.us/client/latest/zoom_amd64.deb"
 
 # Functions
 add_apt_repository() {
     local repo_name=$1 repo_entry=$2 keyring_name=$3 key_url=$4
     curl -fsSL "$key_url" | gpg --dearmor | sudo tee "${KEYRING_DIR}/${keyring_name}-keyring.gpg" > /dev/null
-    echo "deb [signed-by=${KEYRING_DIR}/${keyring_name}-keyring.gpg ] $repo_entry" | sudo tee "${SOURCE_LIST_DIR}/${repo_name}.list" > /dev/null
+    printf "$REPO_FORMAT" "$keyring_name" "$repo_entry" | sudo tee "${SOURCE_LIST_DIR}/${repo_name}.list" > /dev/null
 }
 
 install_deb_from_url() {
