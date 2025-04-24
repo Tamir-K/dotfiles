068285cb47f4d04b940112544025e5620b7d220a
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-04-25 01:35:57 +0300

    Revert to previous model

--- run_on_change_after_install_packages.sh
+++ run_on_change_after_install_packages.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 
+# Constants
 readonly KEYRING_DIR="/usr/share/keyrings"
-readonly REPO_FORMAT="deb [signed-by=${KEYRING_DIR}/%s-keyring.gpg] %s %s %s"
 readonly SOURCE_LIST_DIR="/etc/apt/sources.list.d"
-readonly CUSTOM_REPOS_FILE="${SOURCE_LIST_DIR}/custom_repos.list"
 readonly TEMP_DEB_DIR="/tmp/debfiles"
-readonly UBUNTU_RELEASE=$(grep '^DISTRIB_RELEASE=' /etc/upstream-release/lsb-release | cut -d'=' -f2)   # Get Ubuntu upstream version for Linux Mint
+readonly UBUNTU_RELEASE=$(grep '^DISTRIB_RELEASE=' /etc/upstream-release/lsb-release | cut -d'=' -f2) # Get Ubuntu upstream version for Linux Mint
 readonly UBUNTU_CODENAME=$(grep '^DISTRIB_CODENAME=' /etc/upstream-release/lsb-release | cut -d'=' -f2) # Get Ubuntu upstream codename for Linux Mint
 readonly PACKAGES=(
     git
@@ -21,17 +20,21 @@ readonly PACKAGES=(
     megasync
     nemo-megasync
 )
-readonly REPOS=(
-    "signal-xenial  https://updates.signal.org/desktop/apt xenial main                      signal-desktop          https://updates.signal.org/desktop/apt/keys.asc"
-    "vscodium       https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main  vscodium-archive        https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg"
-    "prebuilt-mpr   https://proget.makedeb.org prebuilt-mpr ${UBUNTU_CODENAME}              prebuilt-mpr-archive    https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub"
-    "megasync       https://mega.nz/linux/repo/xUbuntu ${UBUNTU_RELEASE} ./                 meganz-archive          https://mega.nz/keys/MEGA_signing.key"
-)
+readonly SIGNAL_KEY_URL="https://updates.signal.org/desktop/apt/keys.asc"
+readonly SIGNAL_REPO="https://updates.signal.org/desktop/apt xenial main"
+readonly VSCODIUM_KEY_URL="https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg"
+readonly VSCODIUM_REPO="https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main"
+readonly MPR_KEY_URL="https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub"
+readonly MPR_REPO="https://proget.makedeb.org prebuilt-mpr ${UBUNTU_CODENAME}"
+readonly MEGA_KEY_URL="https://mega.nz/keys/MEGA_signing.key"
+readonly MEGA_REPO="https://mega.nz/linux/repo/xUbuntu_${UBUNTU_RELEASE}/ ./"
 readonly ZOOM_URL="https://zoom.us/client/latest/zoom_amd64.deb"
 
-download_repo_key() {
-    local keyring_name=$1 key_url=$2
-    curl -fsSL "$key_url" | gpg --dearmor | sudo tee "${KEYRING_DIR}/$keyring_name-keyring.gpg" > /dev/null
+# Functions
+add_apt_repository() {
+    local repo_name=$1 repo_entry=$2 keyring_name=$3 key_url=$4
+    curl -fsSL "$key_url" | gpg --dearmor | sudo tee "${KEYRING_DIR}/${keyring_name}-keyring.gpg" > /dev/null
+    echo "deb [signed-by=${KEYRING_DIR}/${keyring_name}-keyring.gpg ] $repo_entry" | sudo tee "${SOURCE_LIST_DIR}/${repo_name}.list" > /dev/null
 }
 
 install_deb_from_url() {
@@ -41,12 +44,15 @@ install_deb_from_url() {
 }
 
 main() {
-    for repo in "${REPOS[@]}"; do
-        download_repo_key $(awk '{print $5, $6}' <<< "$repo")
-    done
-    printf '%s\n' "${REPOS[@]}" | awk -v fmt="$REPO_FORMAT" '{printf fmt "\n", $5, $2, $3, $4}' | sudo tee "${CUSTOM_REPOS_FILE}" > /dev/null
+    add_apt_repository "signal-xenial" "$SIGNAL_REPO" "signal-desktop" "$SIGNAL_KEY_URL"
+    add_apt_repository "vscodium" "$VSCODIUM_REPO" "vscodium-archive" "$VSCODIUM_KEY_URL"
+    add_apt_repository "prebuilt-mpr" "$MPR_REPO" "prebuilt-mpr-archive" "$MPR_KEY_URL"
+    add_apt_repository "megasync" "$MEGA_REPO" "meganz-archive" "$MEGA_KEY_URL"
+
     sudo apt-get update && sudo apt-get install -y "${PACKAGES[@]}"
+
     install_deb_from_url "$ZOOM_URL"
+
     pipx ensurepath && pipx install protonup && protonup -y
 }
 
