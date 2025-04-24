6cf720280b42f3c332a90682e4e2ce56dea379cd
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-04-25 02:26:36 +0300

    Cleanup and auto-cpufreq

--- run_on_change_after_install_packages.sh
+++ run_on_change_after_install_packages.sh
@@ -7,6 +7,7 @@ readonly KEYRING_DIR="/usr/share/keyrings"
 readonly SOURCE_LIST_DIR="/etc/apt/sources.list.d"
 readonly TEMP_DEB_DIR="/tmp/debfiles"
 readonly REPO_FORMAT="deb [arch=amd64 signed-by=${KEYRING_DIR}/%s-keyring.gpg] %s"
+readonly AUTO_CPUFREQ_PATH="/tmp/auto-cpufreq"
 readonly PACKAGES=(
     git
     virtualbox
@@ -26,22 +27,28 @@ readonly SIGNAL_REPO="https://updates.signal.org/desktop/apt xenial main"
 readonly VSCODIUM_KEY_URL="https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg"
 readonly VSCODIUM_REPO="https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main"
 readonly MPR_KEY_URL="https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub"
-readonly MPR_REPO="https://proget.makedeb.org prebuilt-mpr ${DISTRIB_CODENAME}"
+readonly MPR_REPO="https://proget.makedeb.org prebuilt-mpr $DISTRIB_CODENAME"
 readonly MEGA_KEY_URL="https://mega.nz/keys/MEGA_signing.key"
-readonly MEGA_REPO="https://mega.nz/linux/repo/xUbuntu_${DISTRIB_RELEASE}/ ./"
+readonly MEGA_REPO="https://mega.nz/linux/repo/xUbuntu_$DISTRIB_RELEASE/ ./"
 readonly ZOOM_URL="https://zoom.us/client/latest/zoom_amd64.deb"
 
 # Functions
 add_apt_repository() {
     local repo_name=$1 repo_entry=$2 keyring_name=$3 key_url=$4
-    curl -fsSL "$key_url" | gpg --dearmor | sudo tee "${KEYRING_DIR}/${keyring_name}-keyring.gpg" > /dev/null
-    printf "$REPO_FORMAT" "$keyring_name" "$repo_entry" | sudo tee "${SOURCE_LIST_DIR}/${repo_name}.list" > /dev/null
+    curl -fsSL "$key_url" | gpg --dearmor | sudo tee "$KEYRING_DIR/$keyring_name-keyring.gpg" > /dev/null
+    printf "$REPO_FORMAT" "$keyring_name" "$repo_entry" | sudo tee "$SOURCE_LIST_DIR/$repo_name.list" > /dev/null
 }
 
 install_deb_from_url() {
     local package_url=$1
-    curl -fsSL -O --create-dirs --output-dir "${TEMP_DEB_DIR}" "$package_url"
-    sudo apt-get install -y "${TEMP_DEB_DIR}/$(basename "$package_url")"
+    curl -fsSL -O --create-dirs --output-dir "$TEMP_DEB_DIR" "$package_url"
+    sudo apt-get install -y "$TEMP_DEB_DIR/$(basename "$package_url")"
+}
+
+install_auto_cpufreq() {
+    git clone --depth=1 https://github.com/AdnanHodzic/auto-cpufreq.git "$AUTO_CPUFREQ_PATH"
+    sudo "$AUTO_CPUFREQ_PATH/auto-cpufreq-installer"
+    sudo auto-cpufreq --install
 }
 
 main() {
@@ -55,6 +62,8 @@ main() {
     install_deb_from_url "$ZOOM_URL"
 
     pipx ensurepath && pipx install protonup && protonup -y
+
+    install_auto_cpufreq
 }
 
 main
