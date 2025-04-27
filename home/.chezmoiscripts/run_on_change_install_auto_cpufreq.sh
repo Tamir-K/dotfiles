777c931348457161939b12505d74ba241e3b0c2a
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-04-28 00:14:09 +0300

    Add chezmoi root and install script

---
+++ home/.chezmoiscripts/run_on_change_install_auto_cpufreq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Install auto-cpufreq as a daemon from GitHub
+
+set -euo pipefail
+
+readonly AUTO_CPUFREQ_PATH=$(mktemp -d)
+readonly AUTO_CPUFREQ_URL="https://github.com/AdnanHodzic/auto-cpufreq.git"
+
+install_auto_cpufreq() {
+    git clone --depth=1 "${AUTO_CPUFREQ_URL}" "${AUTO_CPUFREQ_PATH}"
+    sudo "${AUTO_CPUFREQ_PATH}/auto-cpufreq-installer"
+    sudo auto-cpufreq --install
+}
+
+cleanup() {
+    rm -rf "${AUTO_CPUFREQ_PATH}"
+}
+
+main() {
+    install_auto_cpufreq
+}
+
+main
+
+trap cleanup EXIT
