fb044c5dace2e562379f45ba67fcde8c574fa25b
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-04-30 04:05:25 +0300

    Remove ProtonGE tools and consolidate scripts

--- .chezmoiscripts/run_on_change_before_bootstrap.sh
+++ .chezmoiscripts/run_on_change_before_bootstrap.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 #
-# Apply basic changes to system
+# Provision system baseline
 
 set -euo pipefail
 
+readonly AUTO_CPUFREQ_PATH=$(mktemp -d)
+readonly AUTO_CPUFREQ_URL="https://github.com/AdnanHodzic/auto-cpufreq.git"
 
-main() {
+# Install package baseline using ansible
+install_packages() {
     sudo apt-get install -y ansible
-    source /etc/upstream-release/lsb-release # Get Ubuntu upstream info for Linux Mint
+    source /etc/upstream-release/lsb-release    # Get Ubuntu upstream info for Linux Mint
     export DISTRIB_RELEASE DISTRIB_CODENAME
     ansible-playbook mint_package_install.yaml
+}
+
+# Install auto-cpufreq daemon from GitHub
+install_auto_cpufreq() {
+    git clone --depth=1 "${AUTO_CPUFREQ_URL}" "${AUTO_CPUFREQ_PATH}"
+    sudo "${AUTO_CPUFREQ_PATH}/auto-cpufreq-installer"
+    sudo auto-cpufreq --install
+}
+
+# Cleanup temporary files
+cleanup() {
+    rm -rf "${AUTO_CPUFREQ_PATH}"
+}
+
+main() {
+    trap cleanup EXIT
+    install_packages
+    install_auto_cpufreq
 }
 
 main
