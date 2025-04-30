b7b29f7ecf6fd214c5e0962d579b0f73dde12e31
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-04-30 12:01:04 +0300

    Add VSCodium extensions install

--- .chezmoiscripts/run_on_change_bootstrap.sh
+++ .chezmoiscripts/run_on_change_bootstrap.sh
@@ -6,11 +6,18 @@ set -euo pipefail
 
 readonly AUTO_CPUFREQ_PATH=$(mktemp -d)
 readonly AUTO_CPUFREQ_URL="https://github.com/AdnanHodzic/auto-cpufreq.git"
+readonly CODIUM_EXTENSIONS=(
+    foxundermoon.shell-format
+    james-yu.latex-workshop
+    ms-python.black-formatter
+    ms-python.debugpy
+    ms-python.python
+)
 
 # Install package baseline using ansible
 install_packages() {
     sudo apt-get install -y ansible
-    source /etc/upstream-release/lsb-release    # Get Ubuntu upstream info for Linux Mint
+    source /etc/upstream-release/lsb-release # Get Ubuntu upstream info for Linux Mint
     export DISTRIB_RELEASE DISTRIB_CODENAME
     ansible-playbook mint_package_install.yaml
 }
@@ -22,6 +29,11 @@ install_auto_cpufreq() {
     sudo auto-cpufreq --install
 }
 
+# Install VSCodium extensions
+install_codium_extensions() {
+    printf "%s\n" "${CODIUM_EXTENSIONS[@]}" | xargs -n 1 codium --install-extension
+}
+
 # Cleanup temporary files
 cleanup() {
     rm -rf "${AUTO_CPUFREQ_PATH}"
@@ -30,6 +42,7 @@ cleanup() {
 main() {
     trap cleanup EXIT
     install_packages
+    install_codium_extensions
     install_auto_cpufreq
 }
 
