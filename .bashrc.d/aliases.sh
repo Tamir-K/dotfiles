62f83a41b380fffde2a7a07e116596531496e9fe
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-08-14 15:02:57 +0300

    Fix aliases color support

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -7,7 +7,7 @@ fi
 
 # Enable color support of ls and grep
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval '$(dircolors -b ~/.dircolors)' || eval '$(dircolors -b)'
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval '$(dircolors -b)'
     alias ls='ls --color=auto'
     alias grep='grep --color=auto'
 fi
