991b4a0e9ebc09e6049b1e40a3c85dac1ba8c7a7
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-08-15 19:13:27 +0300

    Move to fedora

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 # setup.sh
-# Provision baseline for a linux mint system
+# Provision baseline for a Fedora system
 
 set -euo pipefail
 
-source /etc/upstream-release/lsb-release    # Get Ubuntu upstream info for Linux Mint
-source $(pwd)/apt_utils.sh     # Get apt util functions
-
-readonly DEPENDENCIES=(extrepo git)
+readonly RPM_FUSION=(
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+)
+readonly OPENSUSE_BUILD_SERVICE_SIGNAL_REPO="https://download.opensuse.org/repositories/network:im:signal/Fedora_$(rpm -E %fedora)/network:im:signal.repo"
 readonly PACKAGES=(
+    git
     virt-manager
     wireshark
     steam
-    texlive-full
+    texlive-scheme-full
     torbrowser-launcher
     p7zip
     fzf
+    zoxide
     signal-desktop
     codium
     prismlauncher
-)
-readonly MEGA_URL="https://mega.nz/linux/repo/xUbuntu_${DISTRIB_RELEASE}/amd64/megasync-xUbuntu_${DISTRIB_RELEASE}_amd64.deb"
-readonly ZOOM_URL="https://zoom.us/client/latest/zoom_amd64.deb"
-readonly EXTREPO_NAMES=(signal vscodium)
-readonly MPR_REPO=(
-    prebuilt-mpr
-    "https://proget.makedeb.org prebuilt-mpr ${DISTRIB_CODENAME}"
-    prebuilt-mpr-archive
-    https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub
+    "https://mega.nz/linux/repo/Fedora_$(rpm -E %fedora)/x86_64/megasync-Fedora_$(rpm -E %fedora).x86_64.rpm" # MegaSync
+    "https://zoom.us/client/latest/zoom_x86_64.rpm" # Zoom
 )
 readonly CODIUM_EXTENSIONS=(
     foxundermoon.shell-format
@@ -37,21 +33,37 @@ readonly CODIUM_EXTENSIONS=(
     ms-python.python
 )
 
+enable_rpm_fusion() {
+    sudo dnf install -y "${RPM_FUSION[@]}"
+    sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+}
+
+add_vscodium_repo() {
+    sudo tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
+[gitlab.com_paulcarroty_vscodium_repo]
+name=gitlab.com_paulcarroty_vscodium_repo
+baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+metadata_expire=1h
+EOF
+}
+
 install_packages() {
-    sudo apt-get install -y "${DEPENDENCIES[@]}"
-    sudo sed -i 's/# - non-free/- non-free/' /etc/extrepo/config.yaml
-    printf "%s\n" "${EXTREPO_NAMES[@]}" | xargs -n 1 sudo extrepo enable # Enable extrepo repositories
-    add_apt_repository "${MPR_REPO[@]}"
-    sudo apt-get update && sudo apt-get install -y "${PACKAGES[@]}"
+    enable_rpm_fusion
+    sudo dnf config-manager addrepo --from-repofile="${OPENSUSE_BUILD_SERVICE_SIGNAL_REPO}"
+    add_vscodium_repo
+    sudo dnf copr enable -y g3tchoo/prismlauncher
+    sudo dnf install -y "${PACKAGES[@]}"
     sudo usermod -aG libvirt $(whoami) && sudo usermod -aG kvm $(whoami) # Add user to groups needed for virt-manager
-    install_deb_from_url $MEGA_URL
-    install_deb_from_url $ZOOM_URL
     printf "%s\n" "${CODIUM_EXTENSIONS[@]}" | xargs -n 1 codium --install-extension # Install Codium extensions
 }
 
 main() {
     install_packages
-    bash "$(pwd)/scripts/install_auto_cpufreq.sh"
+    ./install_auto_cpufreq.sh
 }
 
 main
