77019d09090067e372effa3fe84169e06db723be
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-08-15 23:04:28 +0300

    Change networking aliases

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -22,9 +22,10 @@ alias la='ls -AF'
 alias ll='ls -lAF'
 
 # More aliases
-alias cls='clear'
-alias ipconfig='ip addr show | awk '\''/^[0-9]+: / {print "Interface " $2} /inet / {print "  IPv4 Address: " $2} /inet6 / {print "  IPv6 Address: " $2}'\'''
 alias ..='cd ..'
+alias cls='clear'
+alias ipconfig='ip addr show'
+alias arp='ip neigh show'
 
 # Git alias for dotfiles management
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
