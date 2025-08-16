506380b7c64d3e99b5b0b7b4991b4529ad0464a7
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-08-16 18:48:11 +0300

    Move history file

--- .bashrc.d/history.sh
+++ .bashrc.d/history.sh
@@ -10,3 +10,5 @@ shopt -s histappend # Append to the history file, don't overwrite it
 HISTCONTROL=ignoreboth # Don't put duplicate lines or lines starting with space in the history.
 HISTSIZE=1000
 HISTFILESIZE=2000
+
+export HISTFILE="${XDG_DATA_HOME}/bash/history" # Move bash history to XDG standard data folder
